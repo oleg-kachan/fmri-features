b8477242f03fc4e373e5964cce4410a85014ce2c
Oleg Kachan <15810673+oleg-kachan@users.noreply.github.com>
2020-04-07 15:35:16 +0300

    Updated spectral features

--- fmri/features/spectral.py
+++ fmri/features/spectral.py
@@ -2,31 +2,45 @@ import numpy as np
 from scipy.linalg import fractional_matrix_power as powm
 
 import networkx as nx
-from fmri.features.sunbeam import nbvals
+#from fmri.features.sunbeam import nbvals
 
 class SpectralFeatureExtractor():
 
-    def spectral_adjacency(self):
-        A = nx.to_numpy_array(self.G)
-        l, _ = np.linalg.eigh(A)
+    def spectral_matrix(self):
+        l, _ = np.linalg.eigh(self.A[0])
         return l
 
-    def spectral_laplacian(self):
-        A = nx.to_numpy_array(self.G)
-        D = np.diag(A.sum(axis=1))
-        L = D - A
-        l, _ = np.linalg.eigh(L)
+    def spectral_laplacian(self, normalized=False):
+
+        # normalized Laplacian
+        if normalized:
+            # deprecated as NetworkX implementation is more stable
+            #A = nx.to_numpy_array(self.G) 
+            #D = np.diag(A.sum(axis=1))
+            #D_pow = powm(D, -0.5)
+            #L = D - A
+            #L_normalized = D_pow @ L @ D_pow
+
+            G = nx.from_numpy_array(self.A[0])
+            L_normalized = np.array(nx.normalized_laplacian_matrix(G).todense())
+            l, _ = np.linalg.eigh(L_normalized)
+
+        # Laplacian
+        else:
+            D = np.diag(self.A[0].sum(axis=1))
+            L = D - self.A[0]
+            l, _ = np.linalg.eigh(L)
+
         return l
 
-    def spectral_laplacian_normalized(self):
-        #A = nx.to_numpy_array(self.G)
-        #D = np.diag(A.sum(axis=1))
-        #D_pow = powm(D, -0.5)
-        #L = D - A
-        #L_normalized = D_pow @ L @ D_pow
-        L_normalized = np.array(nx.normalized_laplacian_matrix(self.G).todense())
-        l, _ = np.linalg.eigh(L_normalized)
-        return l
+    def spectral_poly(self, k_max=3):
+        polynomials = []
+
+        for k in range(2, k_max+1):
+            polynomial_k = np.trace(powm(self.A[0], k))
+            polynomials.append(polynomial_k)
+
+        return np.array(polynomials)
     
     def spectral_non_backtracking(self, topk="automatic"):
         return nbvals(self.G, topk=topk, fmt="1D")
